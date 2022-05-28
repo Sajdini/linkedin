cc1edc7b91ce4a0586fc06d25508db6c6613a647
SamirAjdini <ajdinis1993@gmail.com>
2022-05-28 14:43:17 +0200

    dependencies installed, created Login component

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,11 +1,11 @@
-
-import './App.css';
+import "./App.css";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Login />
+    </>
   );
 }
 
