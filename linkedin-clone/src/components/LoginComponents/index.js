cc193a427dd956dab52f9404e5dd4fd882d63c31
SamirAjdini <ajdinis1993@gmail.com>
2022-06-05 12:27:19 +0200

    imports fixed

---
+++ linkedin-clone/src/components/LoginComponents/index.js
@@ -0,0 +1,6 @@
+export { default as Carousel } from "./Carousel";
+export { default as CTA } from "./CTA";
+export { default as Footer } from "./Footer";
+export { default as Topics } from "./Topics";
+export { default as Hero } from "./Hero";
+export { default as Post } from "./Post";
--- linkedin-clone/src/pages/Login.js
+++ linkedin-clone/src/pages/Login.js
@@ -1,12 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-import Hero from "../components/LoginComponents/Hero";
-import Topics from "../components/LoginComponents/Topics";
+import {
+  Hero,
+  Topics,
+  Post,
+  Carousel,
+  CTA,
+  Footer,
+} from "../components/LoginComponents";
 import NavBar from "../components/NavBar";
-import Post from "../components/LoginComponents/Post";
-import Carousel from "../components/LoginComponents/Carousel";
-import CTA from "../components/LoginComponents/CTA";
-import Footer from "../components/LoginComponents/Footer";
+
 const Login = () => {
   return (
     <Container>
